7590b3c96d21e5334689bb3792c504db36e84cef
Anisul Islam <anisul2010s@yahoo.co.uk>
2024-07-06 08:53:38 +0300

    Update counterSlice.ts

--- src/components/counterSlice.ts
+++ src/components/counterSlice.ts
@@ -1,34 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { initialStateType } from './counterTypes';
 
-type CounterStateType = {
-  count: number;
-};
-
-const initialState: CounterStateType = {
+const initialState: initialStateType = {
   count: 0,
 };
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState: initialState,
+  initialState,
   reducers: {
     increment: (state) => {
-      state.count = state.count + 1;
-    },
-    incrementBy5: (state, action) => {
-      console.log(action);
-      state.count = state.count + action.payload;
-    },
-    decrement: (state) => {
-      state.count = state.count - 1;
+      state.count += 1;
     },
     reset: (state) => {
       state.count = 0;
     },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.count += action.payload;
+    },
   },
 });
 
-export const { increment, decrement, reset, incrementBy5 } =
-  counterSlice.actions;
-
+export const { increment, reset, decrement, incrementByAmount } = counterSlice.actions;
 export default counterSlice.reducer;
